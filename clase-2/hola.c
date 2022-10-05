317d20a61eb37523228ad6da245492fb0fa8dcb7
Erick Alvarez <42850106+becker7000@users.noreply.github.com>
2022-10-05 12:18:23 -0500

    Creando nuestro primer programa en lenguaje C

---
+++ clase-2/hola.c
@@ -0,0 +1,12 @@
+#include<stdio.h> 
+// Nota: stdio significa standard input output
+// .h significa que es un archivo de cabecera (header file)
+
+int main(){ // Sólo puede haber una función main()
+
+    // \n es una secuencia de escape y sirve para dar una salto de línea
+    printf("\n\t Hola a todos desde C"); // \t sirve para dar una tabulación (4 espacios)
+
+    printf("\n\n"); // Mejor formato al programa
+    return 0;
+}
